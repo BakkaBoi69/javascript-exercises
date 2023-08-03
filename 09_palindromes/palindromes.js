fd3e7fdc92b255c47a3a428a9587fd64f66db90c
BakkaBoi69 <98868015+BakkaBoi69@users.noreply.github.com>
2023-08-03 20:46:21 +0530

    Finish 09_palindromes

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,5 +1,7 @@
-const palindromes = function () {
-
+const palindromes = function (str) {
+    str = str.replace(/[^a-zA-Z\d]+/g, "");
+    str = str.toLowerCase();
+    return str.split('').reverse().join('') == str;
 };
 
 // Do not edit below this line
