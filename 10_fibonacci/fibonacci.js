ec621847ad4ba6121c537ec56bf5c8ea2c7beb09
BakkaBoi69 <98868015+BakkaBoi69@users.noreply.github.com>
2023-08-04 09:43:38 +0530

    Finish 10_fibonacci

--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -1,5 +1,16 @@
-const fibonacci = function() {
-
+const fibonacci = function(num) {
+    if (num < 0) {
+        return "OOPS";
+    }
+    num = Number(num);
+    
+    let a = 0, b = 1, temp = b;
+    for (let i = 2; i <= num; i++) {
+        temp = b;
+        b = a + b;
+        a = temp;
+    }
+    return b;
 };
 
 // Do not edit below this line
