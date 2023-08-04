a5e0e779447b4c0d6f0e23e2c09ec5796918142b
BakkaBoi69 <98868015+BakkaBoi69@users.noreply.github.com>
2023-08-04 10:32:29 +0530

    Finish 12_findTheOldest

--- 12_findTheOldest/findTheOldest.js
+++ 12_findTheOldest/findTheOldest.js
@@ -1,5 +1,16 @@
-const findTheOldest = function() {
-
+const findTheOldest = function(people) {
+    let maxAge = -1, oldestPerson;
+    people.forEach(person => {
+        if (person.yearOfDeath == undefined) {
+            maxAge = new Date().getFullYear() - person.yearOfBirth;
+            oldestPerson = person;
+        }
+        else if ((person.yearOfDeath - person.yearOfBirth) > maxAge) {
+            maxAge = person.yearOfDeath - person.yearOfBirth;
+            oldestPerson = person;
+        }
+    });
+    return oldestPerson;
 };
 
 // Do not edit below this line
